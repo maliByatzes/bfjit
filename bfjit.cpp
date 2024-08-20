23c7e93ecdcec72df2baf8ad70bce9f69b7ada41
maliByatzes <malib2027@gmail.com>
2024-08-20 17:59:36 +0200

    read file contents

--- bfjit.cpp
+++ bfjit.cpp
@@ -1,5 +1,9 @@
 #include <cstddef>
+#include <cstdlib>
+#include <fstream>
 #include <iostream>
+#include <stdexcept>
+#include <string>
 
 enum class Token {
     TK_INC = '+',
@@ -17,6 +21,26 @@ struct Command {
     std::size_t operand;
 };
 
+std::string readFileContets(const char* file_path) {
+    std::string file_contets{};
+
+    std::ifstream file(file_path);
+
+    if (file.is_open()) {
+       std::string temp{};
+       while (file) {
+           std::getline(file, temp);
+           if (temp != "") {
+               file_contets += temp;
+           }
+       }
+    } else {
+        throw std::runtime_error("Could not open file");
+    }
+
+    return file_contets;
+}
+
 int main(int argc, char** argv) {
     if (argc != 2) {
         std::cerr << "Not enough arguments.\n";
@@ -25,6 +49,16 @@ int main(int argc, char** argv) {
     }
 
     const char* file_path{ argv[1] };
-    std::cout << "File path: " << file_path << '\n';
+
+    std::string file_contets{};
+    try {
+        file_contets = readFileContets(file_path);
+    } catch (const std::runtime_error &e) {
+        std::cerr << e.what() << '\n';
+        return 1;
+    }
+
+    std::cout << "File contents: " << file_contets << '\n';
+
     return 0;
 }
